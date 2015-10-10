5c1f133f4826a6b5b145ccc11a1b1146d9e6033a
Jannis R <mail@jannisr.de>
2015-10-10 20:50:35 +0200

    moved `self-signed-certificate.zsh` to `scripts/`

---
+++ scripts/self-signed-certificate.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+openssl genrsa -des3 -out ../self-signed.pass.key 2048
+openssl rsa -in ../self-signed.pass.key -out ../self-signed.key
+openssl req -new -key ../self-signed.key -out ../self-signed.csr
+openssl x509 -req -days 365 -in ../self-signed.csr -signkey ../self-signed.key -out ../self-signed.crt
+
+echo "SHA1 fingerprint:\n"
+openssl x509 -in ../self-signed.crt -sha1 -noout -fingerprint
+
+rm ../self-signed.pass.key
+rm ../self-signed.csr
